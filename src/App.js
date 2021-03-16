920ed5f5ff331dfbf044e9159d097b7f9997d26b
yasmeencole <yasmeencole1@mail.com>
2021-03-16 15:35:35 -0500

    Added ApplicationViews.js, added authenication for login and registration. Modified App.js to include route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+import { userStorageKey } from "./components/auth/authSettings"
+import { ApplicationViews } from "./components/ApplicationViews"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export const App = () => (
+  <>
+  <Route
+    render={() => {
+      if (sessionStorage.getItem(userStorageKey)) {
+        return (
+          <>
+        //Components that are rendered when the user is authenticated go inside this React fragment
+          {/* <NavBar /> */}
+          <ApplicationViews />
+      </>
+    )
+  } else {
+    return <Redirect to="/login" />;
+  }
+}}
+/>
+
+  <Route path="/login">
+    <Login />
+  </Route>
+  <Route path="/register">
+    <Register />
+  </Route>
+</>
+)
