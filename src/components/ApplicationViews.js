0c7d923cb8979d171a5609dfa3456f24bac449cd
yasmeencole <yasmeencole1@mail.com>
2021-03-17 11:16:50 -0500

    added route comments in ApplicationViews.js

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,9 +5,35 @@ import { Route } from "react-router-dom"
 export const ApplicationViews = () => {
     return (
     <>
+    {/* http://localhost:8088 */}
     <Route exact path="/">
 
     </Route>
+
+    {/* http://localhost:8088/foods */}
+    {/* <Route path="/">
+
+    </Route> */}
+
+        {/* http://localhost:8088/foods */}
+    {/* <Route path="/">
+
+    </Route> */}
+
+        {/* http://localhost:8088/badfoods */}
+    {/* <Route path="/">
+
+    </Route> */}
+
+            {/* http://localhost:8088/reviews */}
+    {/* <Route path="/">
+
+    </Route> */}
+
+            {/* http://localhost:8088/poops */}
+    {/* <Route path="/">
+
+    </Route> */}
     
     </>
     )
